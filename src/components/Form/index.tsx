2d6b08c1bb2641d6bd859e181fc3f41fb447deae
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-12-07 13:08:26 -0300

    Atualizado

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,29 +1,47 @@
 
 import styles from './Form.module.css';
 
-// type FormInput = {
-//     nome: string;
-//     email: string;
-//     confirmarEmail: string;
-// };
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type FormInput = {
+    name: string;
+    email: string;
+    confirmEmail: string;
+};
 
 import FormLogo from '../../assets/form-logo.png';
 
 function Form() {
+    const {
+        register,
+        handleSubmit,
+        // reset,
+        formState: { errors },
+      } = useForm<FormInput>({
+        // resolver: yupResolver(schema),
+      });
+
+
+      const onSubmit: SubmitHandler<FormInput> = async (userData) => {
+        console.log(userData);
+      };
+
+      console.log(errors);
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
             <img src={FormLogo} alt="imagem-logo" />
             <label>
              Nome
-             <input />
+             <input {...register("name",{ required: true })}/>
+             {errors.name && <span>O nome é obrigatório</span> }
             </label>
             <label>
              E-mail
-             <input />
+             <input {...register("email")}/>
             </label>
             <label>
              Confirmar E-mail
-             <input />
+             <input {...register("confirmEmail")}/>
             </label>
             <button type='submit'>Cadastrar-se</button>
         </form>
