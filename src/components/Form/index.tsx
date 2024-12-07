afb68c7dcd48121e0045c7d94e4dea80e5e6fc87
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2024-12-07 15:27:15 -0300

    Atualizado o Fromulario

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,51 +1,67 @@
-
 import styles from './Form.module.css';
 
 import { useForm, SubmitHandler } from "react-hook-form";
 
-type FormInput = {
-    name: string;
-    email: string;
-    confirmEmail: string;
-};
-
 import FormLogo from '../../assets/form-logo.png';
 
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+type FormInput = {
+  name: string;
+  email: string;
+  confirmEmail: string;
+};
+
+const schema = yup
+.object({
+  name: yup.string().required("O nome é obrigatório"),
+  email: yup.string().email("E-mail inválido").required("O e-mail é obrigatório"),
+  confirmEmail: yup.string().email("E-mail inválido").oneOf([yup.ref('email')], "Os e-mails não coincidem").required("É necessário confirmar o e-mail"),
+  })
+  .required();
+
 function Form() {
-    const {
-        register,
-        handleSubmit,
-        // reset,
-        formState: { errors },
-      } = useForm<FormInput>({
-        // resolver: yupResolver(schema),
-      });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormInput>({
+    resolver: yupResolver(schema),
+  });
 
+  const onSubmit: SubmitHandler<FormInput> = async (userData) => {
+    console.log(userData);
 
-      const onSubmit: SubmitHandler<FormInput> = async (userData) => {
-        console.log(userData);
-      };
+    reset(); // Reseta o formulário após o envio
+  };
 
-      console.log(errors);
-    return (
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <img src={FormLogo} alt="imagem-logo" />
-            <label>
-             Nome
-             <input {...register("name",{ required: true })}/>
-             {errors.name && <span>O nome é obrigatório</span> }
-            </label>
-            <label>
-             E-mail
-             <input {...register("email")}/>
-            </label>
-            <label>
-             Confirmar E-mail
-             <input {...register("confirmEmail")}/>
-            </label>
-            <button type='submit'>Cadastrar-se</button>
-        </form>
-    )
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <img src={FormLogo} alt="Imagem do logo" />
+
+      <label htmlFor="name">
+        Nome
+        <input id="name" {...register("name")} />
+        {errors.name && <span>{errors.name.message}</span>}
+      </label>
+
+      <label htmlFor="email">
+        E-mail
+        <input id="email" {...register("email")} />
+        {errors.email && <span>{errors.email.message}</span>}
+      </label>
+
+      <label htmlFor="confirmEmail">
+        Confirmar E-mail
+        <input id="confirmEmail" {...register("confirmEmail")} />
+        {errors.confirmEmail && <span>{errors.confirmEmail.message}</span>}
+      </label>
+
+      <button type='submit'>Cadastrar-se</button>
+    </form>
+  );
 }
 
 export default Form;
